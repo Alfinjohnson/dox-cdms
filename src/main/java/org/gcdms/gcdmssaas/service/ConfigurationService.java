ca40c5a9eba1638435a076953240216c40e06170
aj <cad01alfin@gmail.com>
2023-10-01 15:44:46 +0530

    added logger

--- src/main/java/org/gcdms/gcdmssaas/service/ConfigurationService.java
+++ src/main/java/org/gcdms/gcdmssaas/service/ConfigurationService.java
@@ -17,31 +17,6 @@ import org.springframework.web.server.ResponseStatusException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
-import reactor.core.publisher.Mono;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.modelmapper.ModelMapper;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
-import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,77 +36,99 @@ public class ConfigurationService {
         this.subscriberService = subscriberService;
     }
 
-    public Mono<CreateConfigurationResponse> createConfiguration(CreateConfigurationRequest createConfigurationRequest) {
+    /**
+     * Create a new configuration with subscribers and return a response.
+     *
+     * @param createConfigurationRequest The request object containing configuration and subscriber data.
+     * @return A Mono containing the CreateConfigurationResponse.
+     */
+    @Transactional
+    public Mono<CreateConfigurationResponse> createConfiguration(@NotNull CreateConfigurationRequest createConfigurationRequest) {
+        log.info("Creating a new configuration...");
         return validateEntityDoesNotExist(createConfigurationRequest.getName())
                 .then(saveConfigurationEntityMono(createConfigurationRequest))
-                .flatMap(configEntity -> {
-                    List<CreatedConfigurationDataModel> createdConfigurationDataModelList = new ArrayList<>();
+                .flatMap(savedConfigurationEntity -> {
+                    List<CreatedConfigurationDataModel> createdConfigurationDataModels = new ArrayList<>();
 
                     return Flux.fromIterable(createConfigurationRequest.getSubscribers())
-                            .flatMap(subscribers -> {
-                                final String subName = subscribers.getName();
-                                final String subType = subscribers.getType();
+                            .flatMap(subscriberRequest -> {
+                                final String subscriberName = subscriberRequest.getName();
+                                final String subscriberType = subscriberRequest.getType();
 
-                                if ("boolean".equals(subType)) {
-                                    final boolean subValue = (boolean) subscribers.getValue();
-                                    return saveToBooleanDataEntity(configEntity, subName, subValue)
-                                            .doOnNext(booleanDataEntity -> booleanDataEntityToResponseMapper(booleanDataEntity, createdConfigurationDataModelList));
+                                if ("boolean".equals(subscriberType)) {
+                                    final boolean subscriberValue = (boolean) subscriberRequest.getValue();
+                                    return saveBooleanDataEntity(savedConfigurationEntity, subscriberName, subscriberValue)
+                                            .doOnNext(savedBooleanDataEntity -> mapToResponse(savedBooleanDataEntity, createdConfigurationDataModels));
                                 }
-                                // Handle other subType cases here
+                                // Handle other subscriber types here
                                 return Mono.empty();
                             })
-                            .then(Mono.just(configEntity))
-                            .map(entity -> CreateConfigurationResponse.builder()
-                                    .id(entity.getId())
-                                    .name(entity.getName())
-                                    .subscribers(createdConfigurationDataModelList)
-                                    .createdUserId(-1L)
-                                    .lastModifiedUserId(-1L)
-                                    .build());
+                            .then(Mono.just(savedConfigurationEntity))
+                            .map(configurationEntity -> createResponse(configurationEntity, createdConfigurationDataModels));
                 })
+                .doOnSuccess(response -> log.info("Configuration successfully created: {}", response.getName()))
+                .doOnError(ex -> log.error("Error occurred during createConfiguration: {}", ex.getMessage(), ex))
                 .onErrorMap(CustomException.class, ex -> {
-                    log.error("Error occurred during createConfigurationMethod: {}", ex.getMessage(), ex);
-                    return new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to complete create the response Create Configuration", ex);
+                    log.warn("Failed to create configuration: {}", ex.getMessage(), ex);
+                    return new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to create configuration", ex);
                 });
     }
 
-    private Mono<Void> validateEntityDoesNotExist(String name) {
+    private @NotNull Mono<Void> validateEntityDoesNotExist(String name) {
         return configurationRepository.findByName(name)
-                .flatMap(entity -> Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Entity already exists name: " + name)))
+                .flatMap(entity -> Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Configuration already exists with name: " + name)))
+                .doOnSuccess(entity -> log.debug("Configuration with name {} already exists", name))
+                .doOnError(ex -> log.debug("Configuration with name {} does not exist", name))
                 .then();
     }
 
-    private Mono<ConfigurationEntity> saveConfigurationEntityMono(CreateConfigurationRequest createConfigurationRequest) {
-        ConfigurationEntity configEntity = new ConfigurationEntity();
-        configEntity.setName(createConfigurationRequest.getName());
-        configEntity.setDescription(createConfigurationRequest.getDescription());
-        configEntity.setCreatedUserId(-1L);
-        configEntity.setLastModifiedUserId(-1L);
+    private @NotNull Mono<ConfigurationEntity> saveConfigurationEntityMono(@NotNull CreateConfigurationRequest createConfigurationRequest) {
+        log.debug("Saving a new configuration entity...");
+        ConfigurationEntity configurationEntity = new ConfigurationEntity();
+        configurationEntity.setName(createConfigurationRequest.getName());
+        configurationEntity.setDescription(createConfigurationRequest.getDescription());
+        configurationEntity.setCreatedUserId(-1L);
+        configurationEntity.setLastModifiedUserId(-1L);
 
-        return configurationRepository.save(configEntity);
+        return configurationRepository.save(configurationEntity)
+                .doOnSuccess(entity -> log.debug("Configuration entity saved with ID: {}", entity.getId()));
     }
 
-    private Mono<BooleanDataEntity> saveToBooleanDataEntity(ConfigurationEntity configEntity, String subName, boolean subValue) {
-        return subscriberService.findIdByName(subName)
+    private @NotNull Mono<BooleanDataEntity> saveBooleanDataEntity(ConfigurationEntity configurationEntity, String subscriberName, boolean subscriberValue) {
+        log.debug("Saving a new boolean data entity...");
+        return subscriberService.findIdByName(subscriberName)
                 .flatMap(subscriberId -> {
                     BooleanDataEntity booleanDataEntity = new BooleanDataEntity();
-                    booleanDataEntity.setConfigurationId(configEntity.getId());
+                    booleanDataEntity.setConfigurationId(configurationEntity.getId());
                     booleanDataEntity.setSubscriberId(subscriberId);
                     booleanDataEntity.setDataType("boolean");
                     booleanDataEntity.setCreatedUserId(-1L);
                     booleanDataEntity.setLastModifiedUserId(-1L);
-                    booleanDataEntity.setValue(subValue);
-                    return booleanDataRepository.save(booleanDataEntity);
+                    booleanDataEntity.setValue(subscriberValue);
+
+                    return booleanDataRepository.save(booleanDataEntity)
+                            .doOnSuccess(entity -> log.debug("Boolean data entity saved with ID: {}", entity.getId()));
                 });
     }
 
-    private void booleanDataEntityToResponseMapper(BooleanDataEntity booleanDataEntity, List<CreatedConfigurationDataModel> createdConfigurationDataModelList) {
+    private void mapToResponse(@NotNull BooleanDataEntity booleanDataEntity, @NotNull List<CreatedConfigurationDataModel> createdConfigurationDataModels) {
         CreatedConfigurationDataModel createdConfigurationDataModel = new CreatedConfigurationDataModel();
         createdConfigurationDataModel.setId(booleanDataEntity.getId());
         createdConfigurationDataModel.setConfigurationId(booleanDataEntity.getConfigurationId());
         createdConfigurationDataModel.setValue(booleanDataEntity.getValue());
         createdConfigurationDataModel.setSubscriberId(booleanDataEntity.getSubscriberId());
         createdConfigurationDataModel.setDataType(booleanDataEntity.getDataType());
-        createdConfigurationDataModelList.add(createdConfigurationDataModel);
+        createdConfigurationDataModels.add(createdConfigurationDataModel);
+    }
+
+    private CreateConfigurationResponse createResponse(@NotNull ConfigurationEntity configurationEntity, List<CreatedConfigurationDataModel> createdConfigurationDataModels) {
+        log.debug("Creating a response object...");
+        return CreateConfigurationResponse.builder()
+                .id(configurationEntity.getId())
+                .name(configurationEntity.getName())
+                .subscribers(createdConfigurationDataModels)
+                .createdUserId(-1L)
+                .lastModifiedUserId(-1L)
+                .build();
     }
 }
